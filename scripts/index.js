786f341dd6bb014c728955ac17154acb94dbdc91
Sergey Burnin <burnin.seregei@yandex.ru>
2024-03-20 15:49:09 +0500

    поменял let на const

--- scripts/index.js
+++ scripts/index.js
@@ -11,15 +11,15 @@
 
 
 const cardTemplate = document.querySelector('#card-template').content;
-let placesList = document.querySelector('.places__list');
+const placesList = document.querySelector('.places__list');
 
 function createCardElem(name, picture, deleteFunction) {
-    let placesItem = cardTemplate.cloneNode('#card-template');
-    let cardImage = placesItem.querySelector('.card__image');
-    let cardTitle = placesItem.querySelector('.card__title');
-    let cardTitleContent = name;
-    let link = picture;
-    let deleteButton = placesItem.querySelector('.card__delete-button')
+    const placesItem = cardTemplate.cloneNode('#card-template');
+    const cardImage = placesItem.querySelector('.card__image');
+    const cardTitle = placesItem.querySelector('.card__title');
+    const cardTitleContent = name;
+    const link = picture;
+    const deleteButton = placesItem.querySelector('.card__delete-button')
     cardTitle.textContent = cardTitleContent;
     cardImage.src =  link;
     cardImage.alt = cardTitleContent;
