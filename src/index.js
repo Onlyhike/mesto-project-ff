7078f0b1b3163fd8e596fa1033fba27630fce538
Sergey Burnin <burnin.seregei@yandex.ru>
2024-05-03 01:02:20 +0500

    last commit

--- src/index.js
+++ src/index.js
@@ -26,7 +26,6 @@ const popupImage = document.querySelector('.popup__image');
 const popupTypeNewAvatar = document.querySelector('.popup_type_new-avatar');
 const popupTypeDeleteConfirm = document.querySelector('.popup_type_delete-confirm');
 const deleteConfirmButton = document.querySelector('.popup__button_delete-confirm');
-
 const validationConfig = {
   formSelector: 'popup__form',
   inputSelector: 'popup__input',
@@ -40,19 +39,20 @@ let matchIdResult;
 
 function deleteListItem(evt) {
   const cardId = evt.target.getAttribute('data-card-to-delete-id');
-  const cardToDelete = document.getElementById(`${cardId}`); //здесь так и не разобрался, почему-то не работает document.querySelector(`#${cardId}`), поэтому оставил getElementById
+  const cardToDelete = document.getElementById(`${cardId}`); //здесь не разобрался почему не работает document.querySelector(`#${cardId}`), поэтому оставил getElementById
+  
   deleteCard(cardId)
-  .then((res) => {
-      if(res.ok) {
+  .then( (res) => {
+      if (res.ok) {
         cardToDelete.remove();
         closePopup(popupTypeDeleteConfirm);
       } else {
-          return Promise.reject(`Ошибка: ${res.status}`)
+          return Promise.reject(`Ошибка: ${res.status}`);
       }
-  })
-  .catch((err) => {
+  } )
+  .catch( (err) => {
       console.log(err);
-  })
+  } )
 };
 
 function setImageTypePopup(evt) {
@@ -64,17 +64,18 @@ function setImageTypePopup(evt) {
 
 function addNewPlace(evt) {
   changeSubmitBtnState(evt);
+
   sendCardData(placeFormField.value, pictureFormField.value)
-  .then((result) => {
+  .then( (result) => {
     matchIdResult = myId === result.owner._id;
     const cardId = result._id;
     const newCard = createCardElem( result.name, result.link, setImageTypePopup, handleLikeClick, 0, matchIdResult, cardId, false, popupTypeDeleteConfirm, deleteConfirmButton);
     placesList.prepend(newCard)
-  })
-  .catch((err) => {
+  } )
+  .catch( (err) => {
       console.log(err);
-    })
-  .finally(() => changeSubmitBtnState(evt));
+    } )
+  .finally( () => changeSubmitBtnState(evt) );
 
   evt.target.reset();
   evt.preventDefault();
@@ -90,15 +91,16 @@ function initEditProfileForm() {
 
 function handleEditFormSubmit(evt) {
   changeSubmitBtnState(evt);
+
   sendProfileData(nameInput.value, descriptionInput.value)
-  .then((result) => {
+  .then( (result) => {
     profileName.textContent = result.name;
     profileDescription.textContent = result.about;
-  })
-  .catch((err) => {
+  } )
+  .catch( (err) => {
       console.log(err);
-    })
-  .finally(() => changeSubmitBtnState(evt));
+    } )
+  .finally( () => changeSubmitBtnState(evt) );
   
   evt.preventDefault();
 
@@ -108,23 +110,23 @@ function handleEditFormSubmit(evt) {
 function changeSubmitBtnState(evt) {
   const loadingText = evt.target.querySelector(`.popup__button_loading-text`);
   const initialText = evt.target.querySelector(`.popup__button_initial-text`);
-  loadingText.classList.toggle('popup_button_text-is-visible')
-  initialText.classList.toggle('popup_button_text-is-visible')
+  loadingText.classList.toggle('popup_button_text-is-visible');
+  initialText.classList.toggle('popup_button_text-is-visible');
 }
 
 deleteConfirmButton.addEventListener('click', deleteListItem);
 
-placesList.addEventListener('click', (evt) => {
-    if(evt.target.classList.contains('card__image')) {
+placesList.addEventListener( 'click', (evt) => {
+    if ( evt.target.classList.contains('card__image') ) {
         openPopup(popupTypeImage);
     }
-})
+} )
 
-profileImage.addEventListener('click', () => {
+profileImage.addEventListener( 'click', () => {
   clearValidation(newAvatarForm, validationConfig);
-  newAvatarForm.reset()
+  newAvatarForm.reset();
   openPopup(popupTypeNewAvatar);
-});
+} );
 
 buttonOpenEditProfileForm.addEventListener( 'click', () => {
   initEditProfileForm(nameInput, profileName, descriptionInput, profileDescription);
@@ -137,33 +139,33 @@ newPlaceform.reset();
 clearValidation(newPlaceform, validationConfig)
 } );
 
-newAvatarForm.addEventListener('submit', (evt) => {
+newAvatarForm.addEventListener( 'submit', (evt) => {
   evt.preventDefault();
   changeSubmitBtnState(evt);
 
   isUrlExist(avatarUrl.value)
-  .then((res) => {
-    if(res.ok && (res.headers.get('Content-Type').startsWith('image'))) {
+  .then( (res) => {
+    if ( res.ok && ( res.headers.get('Content-Type').startsWith('image') ) ) {
       setNewAvatar(res.url)
-      .then((res) => {
+      .then( (res) => {
         profileImage.setAttribute('style', `background-image: url('${res.avatar}')`);
         closePopup(popupTypeNewAvatar);
-      })
+      } )
     } else {
-        return Promise.reject({
+        return Promise.reject( {
           errorMessage: `Ошибка в isUrlExist ${res.status}`,
           mimeType: res.headers.get('Content-Type')
-      })
+      } )
     }
-})
-  .catch((err) => {
+} )
+  .catch( (err) => {
     console.log(err.errorMessage);
-    if(!err.mimeType.startsWith('image')){
-      console.log('По указанному Url не изображение')
+    if (!err.mimeType.startsWith('image')){
+      console.log('По указанному Url не изображение');
     }
-  })
-  .finally(() => changeSubmitBtnState(evt))
-})
+  } )
+  .finally( () => changeSubmitBtnState(evt) )
+} )
 
 editProfileForm.addEventListener( 'submit', (evt) => {
     handleEditFormSubmit(evt);
@@ -175,23 +177,23 @@ newPlaceform.addEventListener( 'submit', (evt) => {
 
 enableValidation(validationConfig);
 
-Promise.all([initProfileSection(), getInitialCards()])
-  .then(([profileRes, cardRes]) => {
+Promise.all( [ initProfileSection(), getInitialCards() ] )
+  .then( ( [profileRes, cardRes] ) => {
     myId = profileRes._id;
 
     profileName.textContent = profileRes.name;
     profileDescription.textContent = profileRes.about;
     profileImage.setAttribute('style', `background-image: url('${profileRes.avatar}')`);
 
-    cardRes.forEach((card) => {
+    cardRes.forEach( (card) => {
       matchIdResult = myId === card.owner._id;
 
-      let isLikedByMe = card.likes.some((likeObj) => {
+      let isLikedByMe = card.likes.some( (likeObj) => {
         return likeObj._id === myId
-      })
+      } )
       placesList.append( createCardElem(card.name, card.link, setImageTypePopup, handleLikeClick, card.likes.length, matchIdResult, card._id, isLikedByMe, popupTypeDeleteConfirm, deleteConfirmButton) )
     } );
-  })
-  .catch((err) => {
+  } )
+  .catch( (err) => {
     console.log(err);
-  });
+  } );
--- src/validation.js
+++ src/validation.js
@@ -3,8 +3,8 @@ export { enableValidation, clearValidation }
 function showInputError(errElement, formField, inputErrorClass) {
     formField.classList.add(inputErrorClass);
 
-    if( formField.validity.patternMismatch ) {
-        formField.setCustomValidity(formField.getAttribute('data-error-message'));
+    if (formField.validity.patternMismatch) {
+        formField.setCustomValidity( formField.getAttribute('data-error-message') );
     } else {
         formField.setCustomValidity('');
     }
@@ -18,17 +18,19 @@ function hideInputError(errElement, formField, inputErrorClass) {
 }
 
 function hasInvalid(form, formFieldSelector) {
-    const formFieldsArr = Array.from(form.querySelectorAll(`.${formFieldSelector}`));
-    return formFieldsArr.some((formField) => {
-        if(!formField.validity.valid) {
-            return true
-        }
-    })
+    const formFieldsArr = Array.from( form.querySelectorAll(`.${formFieldSelector}`) );
+
+    return formFieldsArr.some( (formField) => {
+        if (!formField.validity.valid) {
+            return true;
+        };
+    } )
 }
 
 function toggleButtonState(containsElement, inactiveButtonClass, ButtonClass, formFieldSelector) {
     const button = containsElement.querySelector(`.${ButtonClass}`);
-    if(hasInvalid(containsElement, formFieldSelector)) {
+
+    if ( hasInvalid(containsElement, formFieldSelector) ) {
         button.classList.add(inactiveButtonClass);
         button.setAttribute('disabled', '');
     } else {
@@ -41,34 +43,36 @@ function isValid(form, formField, inputErrorClass) {
     const errElement = form.querySelector(`.${formField.id}-error`);
     
     if(formField.validity.valid) {
-        hideInputError(errElement, formField, inputErrorClass)
+        hideInputError(errElement, formField, inputErrorClass);
     } else {
-        showInputError(errElement, formField, inputErrorClass)
+        showInputError(errElement, formField, inputErrorClass);
     }
 }
 
 function setEventListeners(form, inputErrorClass, inputSelector, inactiveButtonClass, ButtonClass) {
-    const formFieldsList = form.querySelectorAll(`.${inputSelector}`)
+    const formFieldsList = form.querySelectorAll(`.${inputSelector}`);
 
-    formFieldsList.forEach((formField) => formField.addEventListener('input', () => {
+    formFieldsList.forEach( (formField) => formField.addEventListener( 'input', () => {
         isValid(form, formField, inputErrorClass);
-        toggleButtonState(form, inactiveButtonClass, ButtonClass, inputSelector)
-    }))
+        toggleButtonState(form, inactiveButtonClass, ButtonClass, inputSelector);
+    } ) )
 }
 
 function enableValidation(configObj) {
     const formsArr = document.querySelectorAll(`.${configObj.formSelector}`);
 
-    formsArr.forEach((form) => {
+    formsArr.forEach( (form) => {
         setEventListeners(form, configObj.inputErrorClass, configObj.inputSelector, configObj.inactiveButtonClass, configObj.submitButtonSelector)
-    })
+    } )
 }
 
 function clearValidation(form, configObj) {
-    toggleButtonState(form, configObj.inactiveButtonClass, configObj.submitButtonSelector, configObj.inputSelector)
     const formFieldsList = form.querySelectorAll(`.${configObj.inputSelector}`);
-    formFieldsList.forEach((formField) => {
-        const errElement = form.querySelector(`.${formField.id}-error`)
-        hideInputError(errElement, formField, configObj.inputErrorClass)
-    })
+
+    toggleButtonState(form, configObj.inactiveButtonClass, configObj.submitButtonSelector, configObj.inputSelector)
+    formFieldsList.forEach( (formField) => {
+        const errElement = form.querySelector(`.${formField.id}-error`);
+        
+        hideInputError(errElement, formField, configObj.inputErrorClass);
+    } )
 }
